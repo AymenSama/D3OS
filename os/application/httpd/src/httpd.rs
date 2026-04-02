729366bff5194ace203f899d6050828c7bfa9e76
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2026-04-02 14:26:34 +0200

    httpd: Serve files

--- os/application/httpd/src/httpd.rs
+++ os/application/httpd/src/httpd.rs
@@ -6,6 +6,7 @@ use core::{fmt::Display, net::{IpAddr, Ipv6Addr, SocketAddr}};
 
 use alloc::{collections::btree_map::BTreeMap, format, string::String, vec::Vec};
 use httparse::{EMPTY_HEADER, Request};
+use naming::{open, read, shared_types::OpenOptions};
 use network::{NetworkError, TcpListener, TcpStream};
 #[allow(unused_imports)]
 use runtime::*;
@@ -16,7 +17,8 @@ fn main() {
     // ignore all args for now
     let port = 1797;
     let ip = IpAddr::V6(Ipv6Addr::UNSPECIFIED);
-    println!("listening to [{}]:{}", ip, port);
+    let webroot = "/usr/www";
+    println!("serving {} on [{}]:{}", webroot, ip, port);
     
     let mut listener = TcpListener::bind(SocketAddr::new(ip, port))
         .expect("failed to bind socket");
@@ -29,7 +31,7 @@ fn main() {
                 let mut headers = [EMPTY_HEADER; 64];
                 let mut request = Request::new(&mut headers);
                 match request.parse(&buffer[0..len]) {
-                    Ok(_body_start) => if let Err(e) = handle(request).send_to(client) {
+                    Ok(_body_start) => if let Err(e) = handle(request, webroot).send_to(client) {
                         println!("couldn't send reponse to client: {:?}", e);
                     },
                     Err(e) => println!("couldn't parse client request: {:?}", e),
@@ -55,13 +57,23 @@ impl Response {
     }
     
     /// Send this response to a client.
-    fn send_to(self, client: TcpStream) -> Result<(),  NetworkError> {
+    fn send_to(mut self, client: TcpStream) -> Result<(),  NetworkError> {
         client.write(format!("HTTP/1.1 {}\n", self.status).as_bytes())?;
+        self.headers.insert("Content-Length".into(), format!("{}", self.body.len()).into());
         for (header_name, header_value) in self.headers {
             client.write(format!("{}: {}\n", header_name, header_value).as_bytes())?;
         }
         client.write(b"\n")?;
-        client.write(&self.body)?;
+        // we seem to be able to send about 64k at a time, but why?
+        let mut sent = 0;
+        loop {
+            let now_sent = client.write(&self.body[sent..])?;
+            sent += now_sent;
+            //println!("sent {} bytes", sent);
+            if sent == self.body.len() || now_sent == 0 {
+                break;
+            }
+        }
         client.write(b"\n\n")?;
         Ok(())
     }
@@ -70,6 +82,7 @@ impl Response {
 /// HTTP Status Codes as in <https://datatracker.ietf.org/doc/html/rfc9110#name-status-codes>
 enum StatusCode {
     Ok,
+    NotFound,
     MethodNotAllowed,
 }
 
@@ -77,26 +90,51 @@ impl Display for StatusCode {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         write!(f, "{}", match self {
             Self::Ok => "200 OK",
+            Self::NotFound => "404 Not Found",
             Self::MethodNotAllowed => "405 Method Not Allowed",
         })
     }
 }
 
 /// Handle this request, returning a response.
-fn handle(request: Request) -> Response {
+fn handle(request: Request, webroot: &str) -> Response {
     println!("handling request {:?}", request);
-    match request.method.expect("failed to get method") {
-        "GET" => {
-            let mut r = Response::new(StatusCode::Ok);
-            r.headers.insert("Content-Type".into(), "text/plain; charset=UTF-8".into());
-            r.body.extend("Hello from D3OS!".as_bytes());
-            r
-        },
-        method => {
-            let mut r = Response::new(StatusCode::MethodNotAllowed);
-            r.headers.insert("Allowed".into(), "GET".into());
-            r.body.extend(format!("Method not allowed: {method}").as_bytes());
-            r
-        },
+    if request.method == Some("GET") {
+        if let Some(mut request_path) = request.path {
+            request_path = &request_path[1..]; // strip the first /
+            if request_path.is_empty() {
+                request_path = "index.html";
+            }
+            let path = format!("{webroot}/{request_path}");
+            if let Ok(fh) = open(&path, OpenOptions::READONLY) {
+                let mut r = Response::new(StatusCode::Ok);
+                let mut buf: [u8; 4096] = [0; 4096];
+                loop {
+                    let amount = read(fh, &mut buf).expect("failed to read file");
+                    //println!("read {} of {}", amount, request_path);
+                    if amount == 0 {
+                        break;
+                    }
+                    r.body.extend(&buf[0..amount]);
+                }
+                r.headers.insert("Content-Type".into(), match infer::get(&r.body) {
+                    Some(t) => t.mime_type(),
+                    None => {
+                        println!("failed to get mime type for {}, treating it as text", request_path);
+                        "text/plain; charset=UTF-8"
+                    },
+                }.into());
+                return r;
+            }
+        }
+        let mut r = Response::new(StatusCode::NotFound);
+        r.headers.insert("Content-Type".into(), "text/plain; charset=UTF-8".into());
+        r.body.extend("File not found".as_bytes());
+        r
+    } else {
+        let mut r = Response::new(StatusCode::MethodNotAllowed);
+        r.headers.insert("Allowed".into(), "GET".into());
+        r.body.extend(format!("Method not allowed").as_bytes());
+        r
     }
 }
